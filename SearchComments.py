ad8a523ba135ea53cc5f27b22be77a3f22e36e4c
JustinSo1 <sojustin28@gmail.com>
2019-09-22 17:35:40 -0400

    bot searches for new keyword

--- SearchComments.py
+++ SearchComments.py
@@ -2,6 +2,7 @@ import time
 import praw
 import config
 import os
+import requests
 
 
 def bot_login():
@@ -17,9 +18,15 @@ def run_bot(searching_comment, comments_replied):
     print("Obtaining comments")
 
     for comment in searching_comment.subreddit('test').comments(limit=25):
-        if "dog" in comment.body and comment.id not in comments_replied and comment.author != r.user.me():
+        if "!joke" in comment.body and comment.id not in comments_replied and comment.author != r.user.me():
             print("String found!!")
-            comment.reply("I also love dogs! [Here](https://i.imgur.com/Je6bYdS.jpg) is an image of one!")
+
+            comment_reply = "You requested a Chuck Norris joke! Here it is:\n\n"
+            joke = requests.get("https://api.icndb.com/jokes/random").json()['value']['joke']
+            comment_reply += ">" + joke
+            comment_reply += "\n\nThis joke came from [ICNDB.com](https://icndb.com)."
+            comment.reply(comment_reply)
+
             print("Replied to comment " + comment.id)
 
             comments_replied.append(comment.id)
