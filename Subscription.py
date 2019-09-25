5b635f03daaa31a599fa5e54a592fcf79f788ca5
JustinSo1 <sojustin28@gmail.com>
2019-09-25 00:04:47 -0400

    Added same question checking

--- Subscription.py
+++ Subscription.py
@@ -6,19 +6,33 @@ import praw
 def main():
     keyword = "Viberg"
     file_name = 'usernames.txt'
+    questions_file = 'questions.txt'
     # Time delay required by Reddit
     time_delay = 2
 
     # Login for bot
     reddit_bot = authenticate()
     username = get_usernames(file_name)
+    question = questions_database(questions_file)
     subreddit_name = "FrugalMaleFashionCDN"
     subject = keyword + " submission found!"
-    run_bot(reddit_bot, subreddit_name, username, keyword, time_delay, subject)
+    while True:
+        run_bot(reddit_bot, subreddit_name, username, keyword, time_delay, subject, question)
 
 
 def authenticate():
     return praw.Reddit('subscriptionbot', user_agent="subscription bot v01")
+
+
+def questions_database(questions_file):
+    try:
+        with open(questions_file, "r") as file:
+            questions = file.read().split("\n")
+            questions = set(list(filter(None, questions)))
+    except IOError:
+        print("Error: File " + questions_file + " was not found in the current directory")
+        open(questions_file, "w+")
+    return questions
 
 
 # def define_keyword(self, keyword: str):
@@ -46,10 +60,13 @@ def send_message(bot, username, subject, body):
     print("Sent message to  " + username + "!")
 
 
-def run_bot(bot, subreddit, user_list: set, keyword, time_delay, subject):
+def run_bot(bot, subreddit, user_list: set, keyword, time_delay, subject, question):
     print("Obtaining submissions")
     for submission in bot.subreddit(subreddit).hot(limit=30):
-        if keyword in submission.title and submission.author != bot.user.me():
+        if keyword in submission.title and submission.author != bot.user.me() and submission.title not in question:
+            question.add(submission.title)
+            with open("questions.txt", "a") as f:
+                f.write(submission.title + "\n")
             for user in user_list:
                 send_message(bot, user, subject, submission.url)
                 time.sleep(time_delay)
